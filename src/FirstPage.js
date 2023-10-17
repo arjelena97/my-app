6860a628431877dc44b1dbcb3c3e975d2c65b27a
arjelena97 <arjelena97@hotmail.com>
2023-10-18 00:18:57 +0200

    first page created

--- src/FirstPage.js
+++ src/FirstPage.js
@@ -1,15 +1,44 @@
-import { Box } from "@mui/material";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
+import { Box, TextField, Button } from '@mui/material'
 
-const StyledBox = styled(Box)`
-  background-color: #f1e1fa; 
-  height: 200px;
-  width: 200px;  
+const StyledFormBox = styled(Box)`
+  background-color: #f0e4f7;
+  display: grid;
+  justify-content: center;
+  width: 50%;
+  height: 100%;
+  margin-left: 25%;
+  padding-bottom: 20px;
+`;
+
+const Heading = styled.h1`
+// dopuni
 `;
 
 function FirstPage() {
+
   return (
-    <StyledBox>LALA</StyledBox>
+    <StyledFormBox>
+        <Heading>Let's introduce!</Heading>
+        <TextField
+          label="Name"
+          variant="outlined"
+          name="username"
+          margin='normal'
+        />
+        {/* dodaj akcije */}
+        <TextField
+          label="Last Name"
+          variant="outlined"
+          name="last name"
+          margin='normal'
+        />
+        <Button variant="contained" color="primary">
+          Submit
+        </Button>
+        {/* povezi sa sledecom stranom */}
+    </StyledFormBox>
   );
 }
 
