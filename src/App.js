c7ce2b5cd22544ad7ec9aff8c0860c9af73fdd24
arjelena97 <arjelena97@hotmail.com>
2023-10-17 11:34:44 +0200

    styled components imported

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import './App.css';
 import React from 'react';
 import Button from '@mui/material/Button';
+import styled from 'styled-components';
+import { Box } from '@mui/material'
+
+const StyledBox = styled(Box)`
+  heiht: 100%;
+  width: 100%;
+  background-color: #f5c1eb;
+`;
 
 function App() {
   return (
-      <div>
+      <StyledBox>
         <Button 
           variant="contained"
           color="primary"
         >
         Click Me
         </Button>
-      </div>
+      </StyledBox>
   );
 }
 
