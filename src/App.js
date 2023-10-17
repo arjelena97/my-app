ea1308c06302d79c5df11a182a87be9a9254a613
arjelena97 <arjelena97@hotmail.com>
2023-10-17 10:54:13 +0200

    mui imported

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <Button 
+          variant="contained"
+          color="primary"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+        Click Me
+        </Button>
+      </div>
   );
 }
 
