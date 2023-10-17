4a73960484d1e68b6d982f44c66c53ee2eb915d1
arjelena97 <arjelena97@hotmail.com>
2023-10-17 19:58:28 +0200

    welcome page style improved

--- src/App.js
+++ src/App.js
@@ -16,8 +16,12 @@ const Heading = styled.h1`
 `;
 
 const StyledText = styled.p`
-  text-transformation: uppercase;
   margin: 0 20px 20px 20px;
+`;
+
+const ButtonWrapper = styled(Box)`
+  display: flex;
+  justify-content: center;
 `;
 
 function App() {
@@ -26,18 +30,21 @@ function App() {
         <Heading>WELCOME PAGE</Heading>
         <StyledText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</StyledText>
         <StyledText><strong>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</strong></StyledText>
-        <Button 
-          variant="contained"
-          color="primary"
-        >
-        WELCOME
-        </Button>
+        <ButtonWrapper>
+          <Button 
+            variant="contained"
+            color="primary"
+          >
+          WELCOME
+          </Button>
+        </ButtonWrapper>
+        <Box display="flex" width="100%" sx={{ backgroundColor: 'white', justifyContent: 'center', marginTop: '20px' }}>
         <img 
           src="https://static.vecteezy.com/system/resources/previews/006/350/660/non_2x/lavender-horizontal-seamless-pattern-wallpaper-template-with-delicate-purple-flowers-lilac-buds-illustration-flower-road-vector.jpg" 
           alt='flowers'
           height={250}
-          width={1265}
         />
+        </Box>
       </StyledBox>
   );
 }
