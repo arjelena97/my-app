64fe8257c380ecff8a2b4621f7431cc5af25bcd1
arjelena97 <arjelena97@hotmail.com>
2023-10-19 16:49:17 +0200

    palette implemented

---
+++ src/utils/palette.js
@@ -0,0 +1,15 @@
+export const palette = {
+
+  lightBlue: '#daddf7',
+  mediumBlue: '#8f9cf7',
+  darkBlue: '#0f21a6',
+
+  lightRed: '#ef5350',
+  mediumRed: '#d32f2f',
+  darkRed: '#c62828',
+
+  lightGreen: '#4caf50',
+  mediumGreen: '#2e7d32',
+  darkGreen: '#1b5e20',
+
+};
